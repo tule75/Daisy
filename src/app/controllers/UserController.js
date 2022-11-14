db4fd36848ca20e7b28ec2d33b312fa4ceb7fb7c
UIT-21522738 <96582896+UIT-21522738@users.noreply.github.com>
2022-11-14 15:47:55 +0700

    user

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,31 @@
+const User = require('../models/Users')
+var jwt = require('jsonwebtoken');
+
+class UserController {
+    //[GET] /user/:slug
+    show(req, res, next) {
+        if (req.cookies.token){
+            const token = req.cookies.token
+            const id = jwt.verify(token, 'daisy')
+            User.findOne({_id: id})
+            .then(data => {
+                if (data) {
+                    if (data.role === 'customer') {
+                        res.render('customer.html', {customer: data})
+                    } else {
+                        // res.render('seller.html', {customer: data})
+                    }
+                } else {
+                    res.render('login.html', {check: 0})
+                }
+            })
+            .catch(err => {
+                res.send('loi')
+            })
+        } else {
+            res.render('login.html', {check: 0})
+        }    
+    }
+}
+
+module.exports = new UserController();
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,10 @@ const newRouteHome = require("./site")
 const newRouteLogIn = require("./login")
 const newRouteSignup = require("./signup")
 const newRouteGioHang = require("./giohang")
+const newRouteUser = require("./user")
 
 function route(app) {
+    app.use("/user", newRouteUser)
     app.use("/giohang", newRouteGioHang)
     app.use("/signup", newRouteSignup)
     app.use("/login", newRouteLogIn)
