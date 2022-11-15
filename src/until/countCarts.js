062782addbc529bb7b58fbd07c76ab8ae4c81c6c
UIT-21522738 <96582896+UIT-21522738@users.noreply.github.com>
2022-11-16 06:48:27 +0700

    logout and fix

--- src/until/countCarts.js
+++ src/until/countCarts.js
@@ -1,21 +1,21 @@
-const jwt = require('jsonwebtoken')
-const User = require('../app/models/Users')
 const GioHang = require('../app/models/GioHang')
 
 module.exports = {
-    getCounts: function (slug) {
-        const count = 0
-        GioHang.find({user_slug: slug})
+    getCounts: function (slugg) {
+        var count = 0
+        GioHang.find({user_slug: slugg})
         .then(data => {
             if (data) {
                 for (var i = 0; i < data.length; i++) {
-                    count = count + data[i].count;
+                    console.log(data.user_slug)
+                    count += data.count;
                 }
             }
             else {
                 count = count;
             }
         })
-        .catch(err => {res.status(400).send("loi")})
+        .catch(count = 0)
+        return count
     }
 }
