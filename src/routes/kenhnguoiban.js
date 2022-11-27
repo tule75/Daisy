6f90cd582e2b16484c375b815c41b703b48707a5
UIT-21522738 <96582896+UIT-21522738@users.noreply.github.com>
2022-11-27 20:39:40 +0700

    thanh toan

--- src/routes/kenhnguoiban.js
+++ src/routes/kenhnguoiban.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const KNBController = require('../app/controllers/NguoiBanController');
 
+
+router.post("/sendproduct", KNBController.sent)
 router.get("/", KNBController.show);
 
 module.exports = router;
