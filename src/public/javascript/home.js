f929017dd24beb77910c248b4e22e6e85dd534cd
thuydiem2 <iris02093@gmail.com>
2022-11-28 19:34:59 +0700

    add

--- src/public/javascript/home.js
+++ src/public/javascript/home.js
@@ -1,40 +1,40 @@
 
-        var btns = document.querySelectorAll('.btn');
-        var paginationWrapper = document.querySelector('.pagination-wrapper');
-        var bigDotContainer = document.querySelector('.big-dot-container');
-        var littleDot = document.querySelector('.little-dot');
-        const btn = document.querySelector(".btn--next")
-        const popularWrappers = document.querySelectorAll(".popular-info-wrap")
+    //     var btns = document.querySelectorAll('.btn');
+    //     var paginationWrapper = document.querySelector('.pagination-wrapper');
+    //     var bigDotContainer = document.querySelector('.big-dot-container');
+    //     var littleDot = document.querySelector('.little-dot');
+    //     const btn = document.querySelector(".btn--next")
+    //     const popularWrappers = document.querySelectorAll(".popular-info-wrap")
     
 
  
-            for(var i = 0; i < btns.length; i++) {
-            btns[i].addEventListener('click', btnClick);
+    //         for(var i = 0; i < btns.length; i++) {
+    //         btns[i].addEventListener('click', btnClick);
 
-            }
+    //         }
 
-                function btnClick() {
-                // popularWrappers.forEach((index)=> {
-                //     const popularWrapper = popularWrappers[index]
-                if(this.classList.contains('btn--prev') ) {
-                    paginationWrapper.classList.add('transition-prev');
-                } else {
-                    // $(".popular-info-wrap.show").classList.remove("show")
-                    // popularWrapper[index+1].classList.add("show")
-                    paginationWrapper.classList.add('transition-next');  
-                }
-            // })
+    //             function btnClick() {
+    //             // popularWrappers.forEach((index)=> {
+    //             //     const popularWrapper = popularWrappers[index]
+    //             if(this.classList.contains('btn--prev') ) {
+    //                 paginationWrapper.classList.add('transition-prev');
+    //             } else {
+    //                 // $(".popular-info-wrap.show").classList.remove("show")
+    //                 // popularWrapper[index+1].classList.add("show")
+    //                 paginationWrapper.classList.add('transition-next');  
+    //             }
+    //         // })
                 
-                var timeout = setTimeout(cleanClasses, 300);
-                }
+    //             var timeout = setTimeout(cleanClasses, 300);
+    //             }
 
-                function cleanClasses() {
-                if(paginationWrapper.classList.contains('transition-next')) {
-                    paginationWrapper.classList.remove('transition-next')
-                } else if(paginationWrapper.classList.contains('transition-prev')) {
-                    paginationWrapper.classList.remove('transition-prev')
-                }
-    }
+    //             function cleanClasses() {
+    //             if(paginationWrapper.classList.contains('transition-next')) {
+    //                 paginationWrapper.classList.remove('transition-next')
+    //             } else if(paginationWrapper.classList.contains('transition-prev')) {
+    //                 paginationWrapper.classList.remove('transition-prev')
+    //             }
+    // }
 
     // luu vouchers
     const getVouchers = document.querySelectorAll(".coupon-btn")
