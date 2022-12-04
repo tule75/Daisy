35644d2c0141f865171fd18d2f76d452fe52634b
UIT-21522738 <96582896+UIT-21522738@users.noreply.github.com>
2022-12-05 01:15:51 +0700

    customer

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const userController = require('../app/controllers/UserController')
 
+router.post('/took', userController.took)
 router.get('/:slug', userController.show)
 
 module.exports = router
