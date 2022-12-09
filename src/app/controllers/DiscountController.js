9b4637ae43fc87ac90a45c2a94e3e4c92f7eb41a
UIT-21522738 <96582896+UIT-21522738@users.noreply.github.com>
2022-12-09 23:28:50 +0700

    thong bao

--- src/app/controllers/DiscountController.js
+++ src/app/controllers/DiscountController.js
@@ -1,4 +1,6 @@
 const Product = require('../models/Product')
+const Notify = require('../models/Notify')
+const GioHang = require('../models/GioHang')
 const jwt = require('jsonwebtoken')
 
 class DiscountController {
@@ -6,11 +8,48 @@ class DiscountController {
     pDiscount(req, res, next) {
         Product.findOne({slug: req.body.slug})
         .then((product) => {
-            product.price_discount = product.price * (100 - req.body.discount)/100
-            product.save()  
-            .then(() => {res.redirect('/kenhnguoiban')})
-            .catch(() => {res.send('loi')})
+            product.price_discount = product.price * (100 - req.body.discount)/100;
+            GioHang.find({product_slug: req.body.slug})
+            .then((carts) => {
+                if (carts) {
+                    function resolveAfter2Seconds(x) {
+                        return new Promise((resolve) => {
+                          setTimeout(() => {
+                            resolve(x);
+                          }, 500);
+                        });
+                    }
+
+                    var promise = new Promise( function(resolve, reject) {
+                        let pr = [];
+                        carts.forEach((element, i) => {
+                            var no = new Notify({user_slug: element.user_slug, data: `Sản phẩm ${product.name} trong giỏ hàng của bạn đang được giảm giá`})
+                            no.save()
+                            .then(()=> {
+                                pr[i] = 1
+                            })
+                        });
+                        if (pr) {
+                            resolve(pr)
+                        } else {
+                            reject()
+                        }
+                    })
+                    promise.then(async (pr) => {
+                        pr = await resolveAfter2Seconds(pr)
+                        product.save()  
+                        .then(() => {res.status(204).send('xong')})
+                        .catch(() => {res.send('loi')})
+                    })
+                } else {
+                    product.save()  
+                    .then(() => {res.status(204).send('xong')})
+                    .catch(() => {res.send('loi')})
+                }
+            })
+            .catch(() => {res.status(204).send('loi')})
         })
+        .catch(() => {res.status(204).send('loi')})
     }
 
     //[POST] /product/discount/delete
@@ -19,7 +58,7 @@ class DiscountController {
         .then((product) => {
             product.price_discount = product.price;
             product.save()
-            .then(() => {res.redirect('/kenhnguoiban')})
+            .then(() => {res.status(204).send('xong')})
             .catch(() => {res.send('loi')})
         })
     }
