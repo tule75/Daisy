99f30c7c26d31a84c9c85a9396c3d180ae47cacf
UIT-21522738 <96582896+UIT-21522738@users.noreply.github.com>
2023-10-23 22:43:05 +0700

    test push

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -26,6 +26,50 @@ class ProductController {
             res.redirect('login')
         }
     }
+    create(req, res, next) {
+        if (req.cookies.token) {
+            const token = req.cookies.token
+            const id = jwt.verify(token, 'daisy')
+            User.findOne({_id: id})
+            .then(user => {
+                var product = new Product(req.body)
+                product.user_slug = user.slug
+                product.save()
+                .then(() => {
+                    res.status(204).send('add success')
+                })
+                .catch(err => {
+                    res.status(204).send(err)
+                })
+            })
+            .catch(err => {res.status(404).send(err)})
+        } else {
+            res.redirect('login')
+        }
+    }
+    create(req, res, next) {
+        if (req.cookies.token) {
+            const token = req.cookies.token
+            const id = jwt.verify(token, 'daisy')
+            User.findOne({_id: id})
+            .then(user => {
+                var product = new Product(req.body)
+                product.user_slug = user.slug
+                product.save()
+                .then(() => {
+                    res.status(204).send('add success')
+                })
+                .catch(err => {
+                    res.status(204).send(err)
+                })
+            })
+            .catch(err => {res.status(404).send(err)})
+        } else {
+            res.redirect('login')
+        }
+    }
 }
+
+
 
 module.exports = new ProductController()
