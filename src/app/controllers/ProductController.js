ab9b45000a2fbd7ccaeaba44990bdc194ad59608
Lê Tú <96582896+UIT-21522738@users.noreply.github.com>
2023-10-23 22:42:51 +0700

    Update ProductController.js

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -28,4 +28,25 @@ class ProductController {
     }
 }
 
+ create(req, res, next) {
+        if (req.cookies.token) {
+            const token = req.cookies.token
+            const id = jwt.verify(token, 'daisy')
+            User.findOne({_id: id})
+            .then(user => {
+                var product = new Product(req.body)
+                product.user_slug = user.slug
+                product.save()
+                .then(() => {
+                    res.status(204).send('add success')
+                })
+                .catch(err => {
+                    res.status(204).send(err)
+                })
+            })
+            .catch(err => {res.status(404).send(err)})
+        } else {
+            res.redirect('login')
+        }
+    }
 module.exports = new ProductController()
